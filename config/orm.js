4549664a3c3f60f47578b7391b4336f69b995465
Robert Shaw <robertshaw87@gmail.com>
2018-06-06 20:09:01 -0700

    create initial orm logic

---
+++ config/orm.js
@@ -0,0 +1,36 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+var orm = module.exports = {};
+
+orm.all = function (table, callback) {
+    var queryString = "SELECT * FROM ??";
+    connection.query(queryString, [table], function (error, result) {
+        if (error) throw error;
+        callback(result);
+    });
+};
+
+orm.select = function (table, conditionObj, callback) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+    connection.query(queryString, [table, conditionObj], function (error, result) {
+        if (error) throw error;
+        callback(result);
+    });
+};
+
+orm.create = function (table, newObj, callback) {
+    var queryString = "INSERT INTO ?? SET ?";
+    connection.query(queryString, [table, newObj], function (error, result) {
+        if (error) throw error;
+        callback(result);
+    });
+};
+
+orm.update = function (table, conditionObj, updateObj, callback) {
+    var queryString = "UPDATE ?? SET ? WHERE ?";
+    connection.query(queryString, [table, updateObj, conditionObj], function (error, result) {
+        if (error) throw error;
+        callback(result);
+    });
+};
