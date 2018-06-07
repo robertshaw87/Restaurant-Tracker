49538f51d9839d76a2a1c477f18c63aa5c42676f
Robert Shaw <robertshaw87@gmail.com>
2018-06-06 19:35:51 -0700

    add mysql connection (and factor in eventual jawsdb integration with heroku)

--- server.js
+++ server.js
@@ -5,28 +5,25 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+// give access to the public folder
 app.use(express.static("public"));
 
-// parse application/x-www-form-urlencoded
+// use the body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// parse application/json
 app.use(bodyParser.json());
 
-// Set Handlebars.
+// setup handlebars as our render engine
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
+// import and use routes for the server
 var routes = require("./controllers/restaurant_controller.js");
 
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
+// set the server to start listening to our port
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
